ea3042f2ef9dbd1fa1e457a2b41ee5a4adaaef9e
rgold <gold@umd.edu>
2015-01-03 12:12:22 -0500

    uv plane units look good. Added expected horizon scales to all plots. Added zeropadding.

--- rt-imaging.py
+++ rt-imaging.py
@@ -9,6 +9,11 @@
 # distance (8.3 \pm 0.4) kpc
 # lensed image of Sgr A* horizon is 53 \pm 2 microarcs (Ghez et al. 2008; Gillessen et al. 2009a,b)
 # resolution of existing and future mm-VLBI: (30--10) microarcsec 
+#
+# Bardeen 1973, Luminet 1979, Johannsen & Psaltis 2010
+# Shadow diameter:
+# a=0: sqrt(27) R_s
+# a=1: 9/2 R_s
 #########################################################
 
 ## Michaels Notebook ##
@@ -40,6 +45,9 @@ from scipy import *
 from scipy.constants import *
 from scipy import fftpack
 
+angle_unit="arcsec" # "rad"
+zeropadding_factor=4 
+
 pc = scipy.constants.parsec # SI
 G = scipy.constants.G # SI
 c = scipy.constants.c # SI
@@ -51,6 +59,10 @@ d_SagA = 8.3e3*pc
 rad2microarcsec = 360/(2*pi)*3600*1e6
 image_size = 8.15 * (2*rg)/d_SagA * rad2microarcsec
 image_size_rad = 8.15 * (2*rg)/d_SagA
+
+shadow_schwarzschild = sqrt(27)*2*rg # diameter
+shadow_maximally_spinning = 9./2.*2*rg # diameter
+
 
 ## SETTINGS ##
 #nxy = 201 # image resolution (nxy+1)x(nxy+1) see ASTRO_RAY_main.cpp
@@ -78,15 +90,20 @@ fig_pos=["+0+0","+500+0","+0+500","+500+500","+250+250"]
 titles = ["I","Q","U","V"]
 #titles = ["I"]
 
-figure(5) # visibility
+########################
+figure(5) # visibility #
+########################
 
-I_FFT = fftpack.fft2(data[:,:,0])
+I_FFT = fftpack.fft2(data[:,:,0],shape=[nxy*zeropadding_factor,nxy*zeropadding_factor])
 I_FFT = fftpack.fftshift(I_FFT) # low frequencies in center of image
 
 # FIXME: dim-less image length 1 or 2 ?
 # {230.86, 12.2} half-size of the square in a picture plane for each frequency
 # 12.2 what units \muarcsec?
 pixeldim = image_size/nxy # Specify the linear size of a pixel, in \[Mu]as
+if angle_unit=="rad":
+    pixeldim = image_size_rad/nxy
+
 X = pixeldim*arange(-round(nxy/2),round(nxy/2)+1)
 Y = X[:]
 
@@ -96,40 +113,56 @@ freq_unit=1e-9 # uv plane scale
 # uvspacing = 1/(pixeldim*nxy*1e-6 / 3600.*pi/180.)
 # Jon:
 uvspacing = image_size_rad/nxy
-FFT_FREQ = fftfreq(int(nxy+1),d=uvspacing)*freq_unit
-uv = meshgrid(FFT_FREQ,FFT_FREQ)
+
+# 150microarcsec = 1.4 in u-v plane 
+# 15 microarcsec = 14  in u-v plane
+# for plot labels given the
+# nominal scaling of 10^9\lambda for the baseline
+uv_schwarzschild = 150.*1.4/(shadow_schwarzschild/2 /d_SagA * rad2microarcsec)
+uv_maximally_spinning = 150.*1.4/(shadow_maximally_spinning/2 /d_SagA * rad2microarcsec)
+
+FFT_FREQ = fftfreq(shape(I_FFT)[0],d=uvspacing)*freq_unit
 
 I_PSD = np.abs(I_FFT)**2
-pcolormesh(unique(FFT_FREQ),unique(FFT_FREQ),log10(abs(I_FFT)))
+pcolormesh(unique(FFT_FREQ),unique(FFT_FREQ),abs(I_FFT))
 #imshow(log10(I_PSD))
 colorbar()
+clim(0,None)
+axis((-7,7,-7,7))
+gca().set(xlabel="$G\lambda$",ylabel="$G\lambda$")
+gca().add_artist(Circle((0,0),radius = uv_schwarzschild,color="cyan",alpha=0.5,fill=False,lw=4,ls="dashed"))
+gca().add_artist(Circle((0,0),radius = uv_maximally_spinning,color="grey",alpha=0.5,fill=False,lw=4,ls="dashed"))
 
 figure(6) ## Polarization fraction PF = \sqrt{Q^2+U^2}/I ##
 pcolormesh(X,Y,sqrt(data[:,:,1]**2+data[:,:,2]**2)/data[:,:,0])
 # imshow(sqrt(data[:,:,1]**2+data[:,:,2]**2)/data[:,:,0])
 axis((-40,40,-40,40))
+
+gca().add_artist(Circle((0,0),radius = shadow_schwarzschild/2./d_SagA * rad2microarcsec,color="cyan",alpha=0.5,fill=False,lw=4,ls="dashed"))
+gca().add_artist(Circle((0,0),radius = shadow_maximally_spinning/2./d_SagA * rad2microarcsec,color="grey",alpha=0.5,fill=False,lw=4,ls="dashed"))
+
 xlabel(r"$\mu arcsec$");ylabel(r"$\mu arcsec$")
 #xlabel(r"$rad$");ylabel(r"$rad$")
 colorbar()
 title(r"$\sqrt{Q^2+U^2}/I$")
-
+savefig(filename_out.replace(".png","_PF.png"))
 
 ## PLOT ##
 for plot in range(len(titles)):
     figure(plot)
-    #imshow(data[:,:,plot],cmap=colormap[plot])
     pcolormesh(X,Y,data[:,:,plot],cmap=colormap[plot])
     colorbar()
     clim(limits[plot])
-    xlabel(r"$\mu arcsec$")
-    ylabel(r"$\mu arcsec$")
+    gca().add_artist(Circle((0,0),radius = shadow_schwarzschild/2./d_SagA * rad2microarcsec,color="cyan",alpha=0.5,fill=False,lw=4,ls="dashed"))
+    gca().add_artist(Circle((0,0),radius = shadow_maximally_spinning/2./d_SagA * rad2microarcsec,color="grey",alpha=0.5,fill=False,lw=4,ls="dashed"))
+    gca().set(xlabel=r"$\mu arcsec$",ylabel=r"$\mu arcsec$")
     axis((-40,40,-40,40))
     title(titles[plot])
     manager.window.wm_geometry(fig_pos[plot]) # [WIP: need to understand the arg syntax]
     savefig(filename_out.replace(".png","_"+titles[plot]+".png"))
 
-
-
+######## DONE ###
 print "="*8
 print "==DONE=="
 print "="*8
+######## DONE ###
