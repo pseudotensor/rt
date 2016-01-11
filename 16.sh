6caa6a3a01d22e25a606f21476aeec65466f84d8
rgold <gold@umd.edu>
2016-01-11 15:21:54 -0500

    Adjusted filename for 4panel uv plots. Streamlined things. Only run mencoder when found on system.

--- 16.sh
+++ 16.sh
@@ -6,26 +6,31 @@
 # convert \( image1 image2 -append \) \
 # \( image3 image4 -append \) +append result
 
-spin=93.75
-th=100 # 118 # 117 140
-f=230 # 349 230
-N=101 # 199 299
-iter_start=5950 # 5900 # 6200 2150
+spin=93.75 # 92
+th=170 # 157 # 196 # 137
+f=230 # 102 # 230
+N=151 # 199
+iter_start=2140 # 2200
 iter_end=$iter_start
+# case_start=4235484 # 418
+case_start=4442140 # 45502140 
+case_end=$case_start
+
+FILENAME_BASE=shotimag$spin\th$th\f$f\fn
 
 for snapshot in `seq $iter_start 1 $iter_end`; do
-  for case in `seq 71390 1 71390`; do
+  for case in `seq $case_start 1 $case_end`; do
 
-    upper_left_panel=shotimag$spin\th$th\f$f\fn$snapshot\_$N\_IQUV_xy.png
+    upper_left_panel=$FILENAME_BASE$snapshot\case$case\_$N\_IQUV_xy.png
     echo $upper_left_panel
-    upper_right_panel=shotimag$spin\th$th\f$f\fn$snapshot\_$N\_I-LP-EVPA-CP_xy.png
-    lower_left_panel=shotimag$spin\th$th\f$f\fn$snapshot\_$N\_IQUV_uv.png
-    lower_right_panel=shotimag$spin\th$th\f$f\fn$snapshot\_$N\_4panel_uv.png
+    upper_right_panel=$FILENAME_BASE$snapshot\case$case\_$N\_I-LP-EVPA-CP_xy.png
+    lower_left_panel=$FILENAME_BASE$snapshot\case$case\_$N\_IQUV_uv.png
+    lower_right_panel=$FILENAME_BASE$snapshot\case$case\_$N\_miniversion_4panel_uv.png
+
     output_filename=16panel_$snapshot\_$case\.png
 
     convert \( $upper_left_panel $upper_right_panel -append \) \( $lower_left_panel $lower_right_panel -append \) +append $output_filename
 
-    #convert \( shotimag93.75th140f230fn$snapshot\_299_IQUV_xy.png shotimag93.75th140f230fn$snapshot\_299_I-LP-EVPA-CP_xy.png -append \) \( shotimag93.75th140f230fn$snapshot\_299_IQUV_uv.png shotimag93.75th140f230fn$snapshot\_299_4panel_uv.png -append \) +append 16panel_$snapshot\.png
 ######
 done # 
 done #
@@ -33,5 +38,10 @@ done #
 
 #############
 ## ANIMATE ##
-mencoder mf://16panel_????_???.png -mf fps=10:type=png -o movie.mp4 -ovc x264 -of lavf -lavfopts format=mp4
+#############
+
+# credit: http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
+if hash mencoder 2>/dev/null; 
+  then mencoder mf://16panel_????_???.png -mf fps=10:type=png -o movie.mp4 -ovc x264 -of lavf -lavfopts format=mp4
+fi
 #############
