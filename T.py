f52fbb690063e95f121d1093f1c13c76bf7d7855
rgold <gold@umd.edu>
2016-02-02 22:45:04 -0500

    Generalized script for arbitrary resolution

--- T.py
+++ T.py
@@ -1,7 +1,10 @@
 # coding: utf-8
-d=loadtxt(sys.argv[-1],usecols=[3,4,8]).reshape(272,128,256,3)
-loglog(d[:,64,0,2],mean(d[:,64,:,0],axis=1),'k.',label='<T_e>')
-loglog(d[:,64,0,2],mean(d[:,64,:,1],axis=1),'r--',lw=2,label='<T_p>')
+import commands
+FILE=sys.argv[-1]
+n=array(commands.getoutput("tail -n1 "+FILE).split()[:3],dtype=int)+1
+d=loadtxt(FILE,usecols=[3,4,8]).reshape(n[0],n[1],n[2],3)
+loglog(d[:,n[1]/2,0,2],mean(d[:,n[1]/2,:,0],axis=1),'c.',label='<T_e>')
+loglog(d[:,n[1]/2,0,2],mean(d[:,n[1]/2,:,1],axis=1),'r--',lw=2,label='<T_p>')
 legend()
 xlabel(r"$r[M]$",fontsize=18)
 ylabel(r"$T[K]$",fontsize=18)
